6dc50011d9d11c1ecf1b572781d027fd01249ff0
Wolfgang <wolfgang.spindeler@gmail.com>
2021-10-27 11:42:28 +0200

    dynamic model code

---
+++ p73ff_arranging_with_cat_in_the_dat/project/src/models/tree/feature_engineering.py
@@ -0,0 +1,19 @@
+from sklearn.preprocessing import LabelEncoder
+
+
+def feature_pipeline(df):
+    df = encode_objects(df)
+    df = nan_imputation(df)
+
+    return df
+
+
+def encode_objects(df):
+    for col in df.columns:
+        if df[col].dtype == "object":
+            df[col] = LabelEncoder().fit_transform(df[col])
+
+    return df
+
+def nan_imputation(df):
+    return df.fillna(0)
--- p73ff_arranging_with_cat_in_the_dat/project/src/train.py
+++ p73ff_arranging_with_cat_in_the_dat/project/src/train.py
@@ -1,12 +1,12 @@
 import argparse
 import joblib
+import importlib
 import pandas as pd
 from sklearn import metrics
-from sklearn import tree
 
 from config import TRAINING_DATA_PATH
 
-def run(fold):
+def run(model_name, fold):
     """
     runs training on provided fold, i.e. uses the training data with matching fold as test data and trains on remaining
     data.
@@ -17,8 +17,8 @@ def run(fold):
     df_train = pd.read_csv(TRAINING_DATA_PATH)
 
     ######### TODO: handle cats
-    numeric_features = [c for c in df_train.columns if df_train[c].dtype == "float64"]
-    df_train = df_train[numeric_features + ["strat_fold", "target"]]
+    #numeric_features = [c for c in df_train.columns if df_train[c].dtype == "float64"]
+    #df_train = df_train[numeric_features + ["strat_fold", "target"]]
     #########
 
     df_test = df_train[df_train.strat_fold == fold].reset_index(drop=True)
@@ -31,17 +31,23 @@ def run(fold):
     df_test = df_test.drop(["target", "strat_fold"], axis=1)
 
     # TODO: proper nan imputation
-    df_train, df_test = df_train.fillna(0), df_test.fillna(0)
+    #df_train, df_test = df_train.fillna(0), df_test.fillna(0)
 
-    model = tree.DecisionTreeClassifier()
+    feature_engineering = importlib.import_module(f"models.{model_name}.feature_engineering")
+    model = importlib.import_module(f"models.{model_name}.model").model
+
+    df_train = feature_engineering.feature_pipeline(df_train)
+    df_test = feature_engineering.feature_pipeline(df_test)
+
     model.fit(df_train, y_train)
-    predictions = model.predict(df_test)
 
-    auc = metrics.roc_auc_score(y_true=y_test, y_score=predictions)
-    print(f"AUC on fold {fold}: {auc}")
+    auc_test = metrics.roc_auc_score(y_true=y_test, y_score=model.predict(df_test))
+    auc_train = metrics.roc_auc_score(y_true=y_train, y_score=model.predict(df_train))
+    print(f"AUC on fold {fold}: Train: {auc_train}, Test: {auc_test}")
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--fold", type=int)
+    parser.add_argument("--model", type=str)
     args = parser.parse_args()
-    run(fold=args.fold)
+    run(model_name=args.model, fold=args.fold)
